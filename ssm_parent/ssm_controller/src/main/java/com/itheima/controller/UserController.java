23c13400916ee26156402ab9b20a6086131a00b0
AB <17748482017@163.com>
2019-11-17 21:06:52 +0800

    修改Controller

--- ssm_parent/ssm_controller/src/main/java/com/itheima/controller/UserController.java
+++ ssm_parent/ssm_controller/src/main/java/com/itheima/controller/UserController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class UserController {
     public void test01(){
+        //修改
         System.out.println("hello");
+
     }
 
 }
